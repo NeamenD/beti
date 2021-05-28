f72bcc57d9d9df2e055300ad596f784018c5b82e
Neamen_Dobamo <Neamenbirhanu@gmail.com>
2021-05-27 19:13:46 -0600

    works

--- src/main-page/house/index.js
+++ src/main-page/house/index.js
@@ -18,6 +18,7 @@ class house extends Component {
                 <div className="colmd-7">
                    
                      <img src={`https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60`}  alt="House"/>
+                     
                 
                  </div>
                  <div className="col-md-5">
